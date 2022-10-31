55e9edc57c2c3e93952b402c7db0fbc65748d6c4
DrEden33773 <edwardwang33773@gmail.com>
2022-11-01 00:55:34 +0800

    update: single vector serialize and deserialize

--- 297.二叉树的序列化与反序列化.cpp
+++ 297.二叉树的序列化与反序列化.cpp
@@ -34,7 +34,7 @@ class Codec {
     // LevelOrder, with `nullptr` as `#`
 public:
     // Encodes a tree to a single string.
-    vector<vector<string>> serialize(TreeNode* node) {
+    vector<vector<string>> older_serialize(TreeNode* node) {
         vector<vector<string>> res;
 
         // special case
@@ -69,9 +69,43 @@ public:
 
         return res;
     }
+    vector<string> serialize(TreeNode* node) {
+        vector<string> res;
+
+        // special case
+        if (!node) {
+            return res;
+        }
+
+        queue<TreeNode*> queue;
+        queue.push(node);
+
+        while (!queue.empty()) {
+            int currentLevelSize = static_cast<int>(queue.size());
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                // get node
+                auto node = queue.front();
+                // if get nullptr node
+                if (!node) {
+                    queue.pop();
+                    res.push_back("#");
+                    continue;
+                }
+                // remove the node from queue
+                queue.pop();
+                // insert node->val to latest layer_vec
+                res.push_back(to_string(node->val));
+                // join node->left and node->right (include nullptr)
+                queue.push(node->left);
+                queue.push(node->right);
+            }
+        }
+
+        return res;
+    }
 
     // Decodes your encoded data to tree.
-    TreeNode* deserialize(const vector<vector<string>>& data) {
+    TreeNode* older_deserialize(const vector<vector<string>>& data) {
         TreeNode* root = nullptr;
 
         if (data.empty()) {
@@ -114,6 +148,51 @@ public:
 
                 ++num_of_inserted_to_parent;
             }
+        }
+
+        return root;
+    }
+    TreeNode* deserialize(const vector<string>& data) {
+        TreeNode* root = nullptr;
+
+        if (data.empty()) {
+            return nullptr;
+        }
+
+        queue<TreeNode*> queue;
+        bool             if_insert_to_left         = true;
+        TreeNode*        the_parent                = nullptr;
+        int              num_of_inserted_to_parent = -1;
+
+        for (const string& node_info : data) {
+            TreeNode* curr_node = (node_info == "#")
+                ? nullptr
+                : new TreeNode(stoi(node_info));
+
+            queue.push(curr_node);
+
+            if (num_of_inserted_to_parent == 2) {
+                // need to update parent
+                do {
+                    queue.pop();
+                    the_parent = queue.front();
+                } while (!the_parent);
+                num_of_inserted_to_parent = 0;
+            }
+
+            if (!the_parent) {
+                root       = curr_node;
+                the_parent = curr_node;
+            } else {
+                if (if_insert_to_left) {
+                    the_parent->left = curr_node;
+                } else {
+                    the_parent->right = curr_node;
+                }
+                if_insert_to_left = !if_insert_to_left;
+            }
+
+            ++num_of_inserted_to_parent;
         }
 
         return root;
