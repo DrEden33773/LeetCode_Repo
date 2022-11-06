f36d795ad43163eb01cd0363f58f7934556aac30
DrEden33773 <edwardwang33773@gmail.com>
2022-11-06 13:22:18 +0800

    update

---
+++ 1678.设计-goal-解析器.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1678 lang=cpp
+ *
+ * [1678] 设计 Goal 解析器
+ */
+
+// @lc code=start
+
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string interpret(string command) {
+    }
+};
+// @lc code=end
