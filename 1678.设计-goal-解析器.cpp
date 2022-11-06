f7e7c6372d0396e116f50f5263c9931a92171848
DrEden33773 <edwardwang33773@gmail.com>
2022-11-06 19:58:40 +0800

    update

--- 1678.设计-goal-解析器.cpp
+++ 1678.设计-goal-解析器.cpp
@@ -11,8 +11,40 @@
 using namespace std;
 
 class Solution {
+    stack<char> stack;
+    string      res;
+
 public:
-    string interpret(string command) {
+    void operate() {
+        bool if_empty_column = true;
+        while (true) {
+            char curr_char = stack.top();
+            stack.pop();
+            if (curr_char == '(') {
+                break;
+            } else {
+                if_empty_column = false;
+            }
+        }
+        if (if_empty_column) {
+            res += 'o';
+        } else {
+            res += "al";
+        }
+    }
+    string interpret(const string& command) {
+        for (const char& curr_char : command) {
+            if (curr_char != ')') {
+                if (curr_char != 'G') {
+                    stack.push(curr_char);
+                } else {
+                    res += 'G';
+                }
+            } else {
+                operate();
+            }
+        }
+        return res;
     }
 };
 // @lc code=end
